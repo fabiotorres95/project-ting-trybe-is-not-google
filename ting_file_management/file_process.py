75e30d5db9ed077559dffb6e74384045e21f6829
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-28 18:25:04 -0300

    created function remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,14 +3,14 @@ import ting_file_management.file_management as file_management
 
 def process(path_file, instance):
     for i in range(len(instance)):
-        if instance.search(i)["nome_do_arquivo"] == path_file:
+        if instance.search(i)['nome_do_arquivo'] == path_file:
             return
 
     data = file_management.txt_importer(path_file)
     result = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": len(data),
-        "linhas_do_arquivo": data
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data
     }
     instance.enqueue(result)
 
@@ -18,7 +18,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos")
+
+    result = instance.dequeue()
+
+    print(f"Arquivo {result['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
