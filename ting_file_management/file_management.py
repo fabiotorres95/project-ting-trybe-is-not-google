23431e42a53a35f3854e3f511892c28e63db6413
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-28 17:22:30 -0300

    exercicio2 com erro

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,29 @@
+from sys import stderr
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=stderr)
+        return
+    try:
+
+        with open(path_file, "r") as file:
+            return _make_list(file)
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=stderr)
+
+    # except ValueError:
+    #     print("Formato inválido", file=stderr)
+
+
+def _make_list(data):
+    result = []
+    for line in data:
+        line = line.strip()
+        result.append(line)
+
+    return result
+
+
+print(txt_importer("test.csv"))
