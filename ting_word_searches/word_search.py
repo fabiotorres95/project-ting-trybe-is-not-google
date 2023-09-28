276c62ef802e2851df94b291bb9178dd54ecc851
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-28 20:57:43 -0300

    created exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,30 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_lower = word.lower()
+    result = []
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+        path = file['nome_do_arquivo']
+        lines = file['linhas_do_arquivo']
+        for j in range(len(lines)):
+            line = lines[j].lower()
+            if word_lower in line:
+                if len(result) == 0 or result[i]['arquivo'] != path:
+                    result.append({
+                        'palavra': word,
+                        'arquivo': path,
+                        'ocorrencias': [
+                            {
+                                'linha': j + 1
+                            }
+                        ]
+                    })
+                else:
+                    result[i]['ocorrencias'].append({
+                        'linha': j + 1
+                    })
+
+    return result
 
 
 def search_by_word(word, instance):
