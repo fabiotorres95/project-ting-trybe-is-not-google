a2ac26fd8f7450ded74b32e3647d9fb21d42ad59
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-28 18:16:28 -0300

    created function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import ting_file_management.file_management as file_management
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return
+
+    data = file_management.txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+    instance.enqueue(result)
+
+    print(result)
 
 
 def remove(instance):
