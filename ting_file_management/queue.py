cff218cf85e86f124283235fb4db9fc60d3e187e
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-28 15:37:27 -0300

    created methods for class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,25 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
+        self.queue = list()
         self.__len = 0
 
     def __len__(self):
-        return self.__len__
+        return self.__len
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
+        self.__len += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len == 0:
+            return None
+
+        self.__len -= 1
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 > index or index >= self.__len:
+            raise IndexError("Índice Inválido ou Inexistente")
+
+        return self.queue[index]
