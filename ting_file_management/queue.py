8e0e1fd8e49670837f3302a0b122f651de10c8b6
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-28 15:12:02 -0300

    initial commit

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,10 +3,10 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.__len = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.__len__
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
