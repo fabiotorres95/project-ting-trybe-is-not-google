9e560bfcbe7fc7ce6fb709a8074d1d37a0086d26
fabiotorres95 <engfabiotorres@outlook.com>
2023-10-03 05:30:57 -0300

    created test_basic_priority_queueing

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,33 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    x = ['x', 'y', 'z']
+    y = ['a', 'b', 'c', 'x', 'y', 'z']
+    prio_queue = PriorityQueue()
+    prio_queue.enqueue({
+        'nome_do_arquivo': 'alice.txt',
+        'qtd_linhas': len(y),
+        'linhas_do_arquivo': y
+    })
+
+    assert len(prio_queue) == 1
+
+    prio_queue.enqueue({
+        'nome_do_arquivo': 'bob.txt',
+        'qtd_linhas': len(x),
+        'linhas_do_arquivo': x
+    })
+
+    assert len(prio_queue) == 2
+    assert prio_queue.search(0)['nome_do_arquivo'] == 'bob.txt'
+
+    first = prio_queue.dequeue()
+
+    assert len(prio_queue) == 1
+    assert first['nome_do_arquivo'] == 'bob.txt'
+    assert prio_queue.search(0)['nome_do_arquivo'] == 'alice.txt'
+
+    with pytest.raises(IndexError):
+        prio_queue.search(1)
