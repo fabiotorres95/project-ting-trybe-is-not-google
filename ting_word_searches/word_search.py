31dc56504febc1cb20b8aa3b2eb0d09ca76e8aed
fabiotorres95 <engfabiotorres@outlook.com>
2023-10-03 04:40:22 -0300

    created search_by_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -28,15 +28,32 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    data = exists_word(word, instance)
+    word_lower = word.lower()
     result = []
-    for i in range[len(data)]:
-        result.append(data[i])
-        for j in range(len(data[i]['ocorrencias'])):
-            result[i]['ocorrencias'][j]['conteudo'] = instance[
-                file['arquivo']
-            ][
-                'linhas_do_arquivo'
-            ][
-                line['linha'] - 1
-            ]
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+        path = file['nome_do_arquivo']
+        lines = file['linhas_do_arquivo']
+        for j in range(len(lines)):
+            line = lines[j]
+            line_lower = line.lower()
+            if word_lower in line_lower:
+                if len(result) == 0 or result[i]['arquivo'] != path:
+                    result.append({
+                        'palavra': word,
+                        'arquivo': path,
+                        'ocorrencias': [
+                            {
+                                'linha': j + 1,
+                                'conteudo': line
+                            }
+                        ]
+                    })
+                else:
+                    result[i]['ocorrencias'].append({
+                        'linha': j + 1,
+                        'conteudo': line
+                    })
+
+    return result
